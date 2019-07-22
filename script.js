95cb6433487ede6a06b9ca809a0222d6e35a3fec
Calvin Kim <calvinkim4@gmail.com>
2019-07-21 20:55:09 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector('.div1').onclick = function pageFlip() {
+    document.querySelector('.div1').classList.toggle('div-active');
+}
+
+document.querySelector('.div2').onclick = function pageFlip() {
+    document.querySelector('.div2').classList.toggle('div-active');
+}
+
+document.querySelector('.div3').onclick = function pageFlip() {
+    document.querySelector('.div3').classList.toggle('div-active');
+}
+
+document.querySelector('.div4').onclick = function pageFlip() {
+    document.querySelector('.div4').classList.toggle('div-active');
+}
+
+document.querySelector('.div5').onclick = function pageFlip() {
+    document.querySelector('.div5').classList.toggle('div-active');
+}
