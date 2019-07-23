e11b367e2b544640cd9f0781caa531a63e8d5997
Calvin Kim <calvinkim4@gmail.com>
2019-07-23 16:24:11 -0400

    Added assets, finished contact and front page

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 document.querySelector('.div1').onclick = function pageFlip() {
     document.querySelector('.div1').classList.toggle('div-active');
+    document.querySelector('.div1back').classList.toggle('div-active');
+}
+
+document.querySelector('.div1back').onclick = function pageFlip() {
+    document.querySelector('.div1').classList.toggle('div-active');
+    document.querySelector('.div1back').classList.toggle('div-active');
 }
 
 document.querySelector('.div2').onclick = function pageFlip() {
     document.querySelector('.div2').classList.toggle('div-active');
+    document.querySelector('.div2back').classList.toggle('div-active');
 }
+
+document.querySelector('.div2back').onclick = function pageFlip() {
+    document.querySelector('.div2').classList.toggle('div-active');
+    document.querySelector('.div2back').classList.toggle('div-active');
+}
+
 
 document.querySelector('.div3').onclick = function pageFlip() {
     document.querySelector('.div3').classList.toggle('div-active');
+    document.querySelector('.div3back').classList.toggle('div-active');
 }
+
+document.querySelector('.div3back').onclick = function pageFlip() {
+    document.querySelector('.div3').classList.toggle('div-active');
+    document.querySelector('.div3back').classList.toggle('div-active');
+}
+
 
 document.querySelector('.div4').onclick = function pageFlip() {
     document.querySelector('.div4').classList.toggle('div-active');
+    document.querySelector('.div4back').classList.toggle('div-active');
 }
 
-document.querySelector('.div5').onclick = function pageFlip() {
-    document.querySelector('.div5').classList.toggle('div-active');
+document.querySelector('.div4back').onclick = function pageFlip() {
+    document.querySelector('.div4').classList.toggle('div-active');
+    document.querySelector('.div4back').classList.toggle('div-active');
 }
+
+
+// document.querySelector('.div5').onclick = function pageFlip() {
+//     document.querySelector('.div5').classList.toggle('div-active');
+// }
