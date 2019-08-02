a583d1b29100a2788d8d596fa930599e63ed881f
Calvin Kim <calvinkim4@gmail.com>
2019-08-02 14:45:03 -0400

    Finished adding graphics

--- script.js
+++ script.js
@@ -45,9 +45,9 @@ document.querySelector('.div4back').onclick = function pageFlip() {
 //     document.querySelector('.div5').classList.toggle('div-active');
 // }
 
-document.querySelector('.indiv-container').onclick = function (event) {
-    event.stopPropagation();
-}
+// document.querySelector('.indiv-container').onclick = function (event) {
+//     event.stopPropagation();
+// }
 
 let projectLinks = document.getElementsByClassName("indiv-container");
 for(let i = 0; i < projectLinks.length; i++)
@@ -56,3 +56,44 @@ for(let i = 0; i < projectLinks.length; i++)
     event.stopPropagation();
    }
 }
+
+let cohortLinks = document.getElementsByClassName("profile");
+for(let i = 0; i < cohortLinks.length; i++)
+{
+    cohortLinks[i].onclick = function(event) {
+    event.stopPropagation();
+   }
+}
+
+let webAppLinks = document.getElementsByClassName("webapp-links");
+for(let i = 0; i < webAppLinks.length; i++)
+{
+    webAppLinks[i].onclick = function(event) {
+    event.stopPropagation();
+   }
+}
+
+var form = document.getElementsByTagName('form')[0];
+   form.addEventListener('submit',contact,false);
+   function contact(e) {
+      e.preventDefault();
+
+      var target = e.target;
+      var message = '';
+
+      // Loop Through All Input Fields
+      for(let i = 0; i < target.length; ++i) {
+         // Check to make sure it's a value. Don't need to include Buttons
+         if(target[i].type != 'text' && target[i].type != 'textarea') {
+                // Skip to next input since this one doesn't match our rules
+            continue;
+         }
+
+         message +=target[i].value;
+      }
+      // Modify the hidden body input field that is required for the mailto: scheme
+      target.elements["body"].value = message;
+
+      // Submit the form since we previously stopped it. May cause recursive loop in some browsers? Should research this.
+      this.submit();
+   }
