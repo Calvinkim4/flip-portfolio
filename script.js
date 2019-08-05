982caf09dbc3aa0f188d46d073d4a37edde96e87
Calvin Kim <calvinkim4@gmail.com>
2019-08-05 00:49:29 -0400

    Changed font size in mobile

--- script.js
+++ script.js
@@ -46,73 +46,19 @@ if (windowWidth >= 1000) {
     }
 } else {
     document.querySelector('.right-btn').onclick = function pageFlip() {
-        if (page <= 8) {
+        if (page <= 7) {
             page++;
             document.getElementById(page).classList.toggle('div-active-mobile');
-            // document.getElementById(page+1).classList.toggle('div-active-mobile');
-            // if (page % 2 !== 0) {
-            //     document.getElementById(page).style.zIndex = '-10';
-            //     document.getElementById(page - 1).style.zIndex = '-10';
-            //     console.log(page);
-            // }
         }
     }
 
     document.querySelector('.left-btn').onclick = function pageFlip() {
         if (page >= 1) {
             document.getElementById(page).classList.toggle('div-active-mobile');
-            // document.getElementById(page+1).classList.toggle('div-active-mobile'); 
-            // if (page % 2 !== 0) {
-            //     document.getElementById(page).style.zIndex = '10';
-            //     document.getElementById(page - 1).style.zIndex = '10';
-            // } 
             page--;
         }
         
     }
-
-
-    // document.querySelector('.div1').onclick = function pageFlip() {
-    //     document.querySelector('.div1').classList.toggle('div-active-mobile');
-    //     // document.querySelector('.div1back').classList.toggle('div-active-mobile');
-    // }
-
-    // document.querySelector('.div1back').onclick = function pageFlip() {
-    //     // document.querySelector('.div1').classList.toggle('div-active-mobile');
-    //     document.querySelector('.div1back').classList.toggle('div-active-mobile');
-    // }
-
-    // document.querySelector('.div2').onclick = function pageFlip() {
-    //     document.querySelector('.div2').classList.toggle('div-active-mobile');
-    //     // document.querySelector('.div2back').classList.toggle('div-active-mobile');
-    // }
-
-    // document.querySelector('.div2back').onclick = function pageFlip() {
-    //     // document.querySelector('.div2').classList.toggle('div-active-mobile');
-    //     document.querySelector('.div2back').classList.toggle('div-active-mobile');
-    // }
-
-
-    // document.querySelector('.div3').onclick = function pageFlip() {
-    //     document.querySelector('.div3').classList.toggle('div-active-mobile');
-    //     // document.querySelector('.div3back').classList.toggle('div-active-mobile');
-    // }
-
-    // document.querySelector('.div3back').onclick = function pageFlip() {
-    //     // document.querySelector('.div3').classList.toggle('div-active-mobile');
-    //     document.querySelector('.div3back').classList.toggle('div-active-mobile');
-    // }
-
-
-    // document.querySelector('.div4').onclick = function pageFlip() {
-    //     document.querySelector('.div4').classList.toggle('div-active-mobile');
-    //     // document.querySelector('.div4back').classList.toggle('div-active-mobile');
-    // }
-
-    // document.querySelector('.div4back').onclick = function pageFlip() {
-    //     // document.querySelector('.div4').classList.toggle('div-active-mobile');
-    //     document.querySelector('.div4back').classList.toggle('div-active-mobile');
-    // }
 }
 
 
