be364a37d8fd28472b6c79b0e92dbacc5f689302
Calvin Kim <calvinkim4@gmail.com>
2019-08-04 22:10:58 -0400

    Added media queries, buttons, flips

--- script.js
+++ script.js
@@ -1,44 +1,134 @@
-document.querySelector('.div1').onclick = function pageFlip() {
-    document.querySelector('.div1').classList.toggle('div-active');
-    document.querySelector('.div1back').classList.toggle('div-active');
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+
+if (windowWidth >= 1000) {
+    document.querySelector('.div1').onclick = function pageFlip() {
+        document.querySelector('.div1').classList.toggle('div-active');
+        document.querySelector('.div1back').classList.toggle('div-active');
+    }
+
+    document.querySelector('.div1back').onclick = function pageFlip() {
+        document.querySelector('.div1').classList.toggle('div-active');
+        document.querySelector('.div1back').classList.toggle('div-active');
+    }
+
+    document.querySelector('.div2').onclick = function pageFlip() {
+        document.querySelector('.div2').classList.toggle('div-active');
+        document.querySelector('.div2back').classList.toggle('div-active');
+    }
+
+    document.querySelector('.div2back').onclick = function pageFlip() {
+        document.querySelector('.div2').classList.toggle('div-active');
+        document.querySelector('.div2back').classList.toggle('div-active');
+    }
+
+
+    document.querySelector('.div3').onclick = function pageFlip() {
+        document.querySelector('.div3').classList.toggle('div-active');
+        document.querySelector('.div3back').classList.toggle('div-active');
+    }
+
+    document.querySelector('.div3back').onclick = function pageFlip() {
+        document.querySelector('.div3').classList.toggle('div-active');
+        document.querySelector('.div3back').classList.toggle('div-active');
+    }
+
+
+    document.querySelector('.div4').onclick = function pageFlip() {
+        document.querySelector('.div4').classList.toggle('div-active');
+        document.querySelector('.div4back').classList.toggle('div-active');
+    }
+
+    document.querySelector('.div4back').onclick = function pageFlip() {
+        document.querySelector('.div4').classList.toggle('div-active');
+        document.querySelector('.div4back').classList.toggle('div-active');
+    }
+} else {
+    // document.querySelector('.div1').onclick = function pageFlip() {
+    //     document.querySelector('.div1').classList.toggle('div-active-mobile');
+    //     document.querySelector('.div1back').classList.toggle('div-active-mobile');
+    // }
+
+    // document.querySelector('.div1back').onclick = function pageFlip() {
+    //     document.querySelector('.div1').classList.toggle('div-active-mobile');
+    //     document.querySelector('.div1back').classList.toggle('div-active-mobile');
+    // }
+
+    // document.querySelector('.div2').onclick = function pageFlip() {
+    //     document.querySelector('.div2').classList.toggle('div-active-mobile');
+    //     document.querySelector('.div2back').classList.toggle('div-active-mobile');
+    // }
+
+    // document.querySelector('.div2back').onclick = function pageFlip() {
+    //     document.querySelector('.div2').classList.toggle('div-active-mobile');
+    //     document.querySelector('.div2back').classList.toggle('div-active-mobile');
+    // }
+
+
+    // document.querySelector('.div3').onclick = function pageFlip() {
+    //     document.querySelector('.div3').classList.toggle('div-active-mobile');
+    //     document.querySelector('.div3back').classList.toggle('div-active-mobile');
+    // }
+
+    // document.querySelector('.div3back').onclick = function pageFlip() {
+    //     document.querySelector('.div3').classList.toggle('div-active-mobile');
+    //     document.querySelector('.div3back').classList.toggle('div-active-mobile');
+    // }
+
+
+    // document.querySelector('.div4').onclick = function pageFlip() {
+    //     document.querySelector('.div4').classList.toggle('div-active-mobile');
+    //     document.querySelector('.div4back').classList.toggle('div-active-mobile');
+    // }
+
+    // document.querySelector('.div4back').onclick = function pageFlip() {
+    //     document.querySelector('.div4').classList.toggle('div-active-mobile');
+    //     document.querySelector('.div4back').classList.toggle('div-active-mobile');
+    // }
+
+    document.querySelector('.div1').onclick = function pageFlip() {
+        document.querySelector('.div1').classList.toggle('div-active');
+        document.querySelector('.div1back').classList.toggle('div-active');
+    }
+
+    document.querySelector('.div1back').onclick = function pageFlip() {
+        document.querySelector('.div1').classList.toggle('div-active');
+        document.querySelector('.div1back').classList.toggle('div-active');
+    }
+
+    document.querySelector('.div2').onclick = function pageFlip() {
+        document.querySelector('.div2').classList.toggle('div-active');
+        document.querySelector('.div2back').classList.toggle('div-active');
+    }
+
+    document.querySelector('.div2back').onclick = function pageFlip() {
+        document.querySelector('.div2').classList.toggle('div-active');
+        document.querySelector('.div2back').classList.toggle('div-active');
+    }
+
+
+    document.querySelector('.div3').onclick = function pageFlip() {
+        document.querySelector('.div3').classList.toggle('div-active');
+        document.querySelector('.div3back').classList.toggle('div-active');
+    }
+
+    document.querySelector('.div3back').onclick = function pageFlip() {
+        document.querySelector('.div3').classList.toggle('div-active');
+        document.querySelector('.div3back').classList.toggle('div-active');
+    }
+
+
+    document.querySelector('.div4').onclick = function pageFlip() {
+        document.querySelector('.div4').classList.toggle('div-active');
+        document.querySelector('.div4back').classList.toggle('div-active');
+    }
+
+    document.querySelector('.div4back').onclick = function pageFlip() {
+        document.querySelector('.div4').classList.toggle('div-active');
+        document.querySelector('.div4back').classList.toggle('div-active');
+    }
 }
 
-document.querySelector('.div1back').onclick = function pageFlip() {
-    document.querySelector('.div1').classList.toggle('div-active');
-    document.querySelector('.div1back').classList.toggle('div-active');
-}
-
-document.querySelector('.div2').onclick = function pageFlip() {
-    document.querySelector('.div2').classList.toggle('div-active');
-    document.querySelector('.div2back').classList.toggle('div-active');
-}
-
-document.querySelector('.div2back').onclick = function pageFlip() {
-    document.querySelector('.div2').classList.toggle('div-active');
-    document.querySelector('.div2back').classList.toggle('div-active');
-}
-
-
-document.querySelector('.div3').onclick = function pageFlip() {
-    document.querySelector('.div3').classList.toggle('div-active');
-    document.querySelector('.div3back').classList.toggle('div-active');
-}
-
-document.querySelector('.div3back').onclick = function pageFlip() {
-    document.querySelector('.div3').classList.toggle('div-active');
-    document.querySelector('.div3back').classList.toggle('div-active');
-}
-
-
-document.querySelector('.div4').onclick = function pageFlip() {
-    document.querySelector('.div4').classList.toggle('div-active');
-    document.querySelector('.div4back').classList.toggle('div-active');
-}
-
-document.querySelector('.div4back').onclick = function pageFlip() {
-    document.querySelector('.div4').classList.toggle('div-active');
-    document.querySelector('.div4back').classList.toggle('div-active');
-}
 
 let projectLinks = document.getElementsByClassName("indiv-container");
 for(let i = 0; i < projectLinks.length; i++)
